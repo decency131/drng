87f73872fd5a51ecf12dec56511cc9a64933dc80
decency131 <68605507+decency131@users.noreply.github.com>
2024-04-24 13:18:54 +0200

    Fixes
    numbers are funnier
    supports simultaneous calls now

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 use std::time::SystemTime;
 
-fn rng(min: u64, max: u64) -> u64{
+fn rng(min: u64, max: u64) -> u128{
     match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
-        Ok(n) => (n.as_secs() * 3543776743 + 3576876877) % (max-min+1 ) + min,
+        Ok(n) => (n.as_nanos() * 354377617451454163 + 35768279713976877) % ((max-min+1) as u128) + (min as u128),
         Err(_) => panic!("SystemTime before UNIX EPOCH!"),
     }
 }
 
-fn rng_seq(min: u64, max: u64, len: usize) -> Vec<u64>{
+fn rng_seq(min: u64, max: u64, len: usize) -> Vec<u128>{
     match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
         Ok(n) => {
-            let mut seq: Vec<u64> = Vec::with_capacity(len);
-            let mut x = n.as_secs();
+            let mut seq: Vec<u128> = Vec::with_capacity(len);
+            let mut x = n.as_nanos();
             for _i in 0..len {
-                x = (x * 3543776743 + 3576876877) % (max-min+1 ) + min;
+                x = (x * 354377617451454163 + 35768279713976877) % ((max-min+1) as u128) + (min as u128);
                 seq.push(x);
             }
             seq
